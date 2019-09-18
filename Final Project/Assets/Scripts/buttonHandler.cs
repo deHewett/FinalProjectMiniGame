bd7d3de6e688a3036d9a688e0798bf493cc122f9
deHewett <42918243+deHewett@users.noreply.github.com>
2019-09-19 09:08:23 +1200

    current

---
+++ Final Project/Assets/Scripts/buttonHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonHandler : MonoBehaviour
+{
+    Scene currentScene;
+    string sceneName;
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
+
+    // Update is called once per frame
+    public void nextScene()
+    {
+        if(sceneName == "1")
+        {
+            SceneManager.LoadScene("2");
+        }
+        if (sceneName == "2")
+        {
+            SceneManager.LoadScene("3");
+        }
+        if (sceneName == "3")
+        {
+            Application.Quit();
+        }
+    }
+}
--- Final Project/Assets/Scripts/gameController.cs
+++ Final Project/Assets/Scripts/gameController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class gameController : MonoBehaviour
 {
-    public int score;
-    public int combo;
-    public int lives;
-    private int count;
+    public int score = 0;
+    public int combo = 1;
+    public int lives = 3;
+    private int count = 0;
     public GameObject endScreen;
-    public Text finalScoreText;
+    public Text round1score;
+    public Text round2score;
+    public Text round3score;
+    public Text final;
     public Text rewardText;
+
+    public int round1=0;
+    public int round2=0;
+    public int round3 = 0;
+    public int finalScore=0;
 
     private string rewardS = "SMALL";
     private string rewardM = "MEDIUM";
@@ -20,43 +29,77 @@ public class gameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this);
         score = 0;
-        combo = 1;
-        lives = 3;
-        count = 0;
-        Scene[] scenes = SceneManager.GetAllScenes();
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(score > 25000)
-        {
-            SceneManager.
-        }
         if(lives <= 0)
         {
-            
             endScreen.SetActive(true);
-            finalScoreText.text = score.ToString();
+            if (round1 == 0)
+            {
+                round1 = score;
+                round1score.text = round1.ToString();
+                getReward();
+                finalScore = round1;
+                final.text = finalScore.ToString();
+                Time.timeScale = 0;
+
+            }
+            else 
+            {
+                if (round2 == 0) {
+                    round2 = score;
+                    round2score.text = round2.ToString();
+                    getReward();
+                    finalScore += round2;
+                    final.text = finalScore.ToString();
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    if (round3 == 0)
+                    {
+                        round3 = score;
+                        round3score.text = round3.ToString();
+                        getReward();
+                        finalScore += round2;
+                        final.text = finalScore.ToString();
+                        Time.timeScale = 0;
+
+                    }
+                }        
+            }
+
+            
+            
             
            
-            if (score > 20000)
-            {
-                rewardText.text = rewardL;
-            }
-            else if (score > 5000)
-            {
-                rewardText.text = rewardM;
-            }
-            else if (score > 1000)
-            {
-                rewardText.text = rewardS;
-            }
-            else { rewardText.text = "Score higher to earn a reward"; }
-            Time.timeScale = 0;
+            
+            
 
         }
+    }
+
+    void getReward()
+    {
+        if (score > 20000)
+        {
+            rewardText.text = rewardL;
+        }
+        else if (score > 5000)
+        {
+            rewardText.text = rewardM;
+        }
+        else if (score > 1000)
+        {
+            rewardText.text = rewardS;
+        }
+        else { rewardText.text = "Score higher to earn a reward"; }
     }
 
     public void addScore()
